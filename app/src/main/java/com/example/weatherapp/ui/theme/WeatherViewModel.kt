0d433943f835f69e58ee9735dbee5ffc37d37ebd
MDCetinka <melissa.dchetinkaya@gmail.com>
2026-08-19 22:56:43 +0300

    Update WeatherViewModel.kt

--- app/src/main/java/com/example/weatherapp/ui/theme/WeatherViewModel.kt
+++ app/src/main/java/com/example/weatherapp/ui/theme/WeatherViewModel.kt
@@ -1,10 +1,31 @@
 package com.example.weatherapp.ui.theme
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.weatherapp.WeatherApi
 import com.example.weatherapp.WeatherResponse
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
-class WeatherViewModel : ViewModel(){ //extending viewmodel
+class WeatherViewModel : ViewModel(){ //extending viewmodel, UI related data,
+    //ekranı yan döndürmek gibi konfigürasyon değişimlerinde data yamulmasın
     private val _weatherData = MutableStateFlow<WeatherResponse?>(null)
+    //mutable == değişebilir, stream of data, _backing propertyler içinmiş.
+//null initializelanıyor daha data gelmeden böylece UI yüklenme ekranı vs gösterebilir
 
+    val weatherData: StateFlow<WeatherResponse?> = _weatherData
+    
+    private val weatherApi = WeatherApi.create()
+
+    fun fetchWeather(city: String, apiKey: String){
+        viewModelScope.launch{
+            try{
+                val response = weatherApi.getWeather(city, apiKey)
+                _weatherData.value = response
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
 }
